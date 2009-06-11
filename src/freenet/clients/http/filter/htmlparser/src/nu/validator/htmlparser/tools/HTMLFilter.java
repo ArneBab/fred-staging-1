c3547da0939b5765a5e17e37f21bf05b1af41ac5
kurmiashish <kurmiashish@gmail.com>
2009-06-11 22:46:57 +0530

    tweaked html 5 parser. Original html 5 parser is not actually a whitelist based parser in the sense it allows all tags(whether valid html tag or not). This has been tweaked to be a whitelist based filter. Check freenet.clients.http.filter.htmlparser.src.nu.validator.htmlparser.tools.HTMLFilter . Right now it allows all the html tags ofcourse we want to suppress few tags which might reveal the information about the user. Attribute filtering needs to be done. Also check nu.validator.htmlparser.imple.Tokenizer. This module tokenizes the html document and calls implementation of tokenhandler interface.

---
+++ src/freenet/clients/http/filter/htmlparser/src/nu/validator/htmlparser/tools/HTMLFilter.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2008 Mozilla Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package nu.validator.htmlparser.tools;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.MalformedURLException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import nu.validator.htmlparser.common.XmlViolationPolicy;
+import nu.validator.htmlparser.sax.HtmlParser;
+import nu.validator.htmlparser.sax.HtmlSerializer;
+import nu.validator.htmlparser.sax.XmlSerializer;
+
+import nu.validator.htmlparser.test.SystemErrErrorHandler;
+
+import org.xml.sax.ContentHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+public class HTMLFilter {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws SAXException,
+            ParserConfigurationException, MalformedURLException, IOException,
+            TransformerException {
+        InputStream in;
+        OutputStream out;
+      
+        in = new FileInputStream("/home/ashish/search.html");
+        out = new FileOutputStream("/home/ashish/searchparse.html");
+        ContentHandler serializer = new HtmlSerializer(out);
+
+        HtmlParser parser = new HtmlParser(XmlViolationPolicy.ALLOW);
+        parser.setStreamabilityViolationPolicy(XmlViolationPolicy.ALLOW);
+        parser.setErrorHandler(new SystemErrErrorHandler());
+        parser.setContentHandler(serializer);
+        parser.setProperty("http://xml.org/sax/properties/lexical-handler",
+                serializer);
+        parser.parse(new InputSource(in));
+        out.flush();
+        out.close();
+    }
+}
