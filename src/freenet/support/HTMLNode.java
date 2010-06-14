031f14dc8c40e1d5b6c1fa357babf1e8c7417fcb
Spencer Jackson <spencerandrewjackson@gmail.com>
2010-06-13 22:58:56 -0400

    Fix potential NPE

--- src/freenet/support/HTMLNode.java
+++ src/freenet/support/HTMLNode.java
@@ -58,7 +58,7 @@ public class HTMLNode implements XMLCharacterClasses {
 				throw new IllegalArgumentException("attribute names and values differ in length");
 			}
 			for (int attributeIndex = 0, attributeCount = attributeNames.length; attributeIndex < attributeCount; attributeIndex++) {
-				if(attributeNames[attributeIndex].equalsIgnoreCase("")) continue;
+				if(attributeNames[attributeIndex] != null && attributeNames[attributeIndex].isEmpty()) continue;
 				if ((attributeNames[attributeIndex] == null) || !checkNamePattern(attributeNames[attributeIndex])) {
 					throw new IllegalArgumentException("attributeName is not legal");
 				}
