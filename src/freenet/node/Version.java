38b4b100c901eb942dcd9e424b6c94166284e3c9
Justus Ranvier <justusranvier@tormail.org>
2012-10-06 20:23:24 -0500

    Build 2

--- src/freenet/node/Version.java
+++ src/freenet/node/Version.java
@@ -56,7 +56,7 @@ public class Version {
 
     /** The build number of the current revision. for official releases it should
      *  be the same as buildNumber */
-	private static final int buildNumber = 1;
+	private static final int buildNumber = 2;
 	
     /** Oldest build of Fred we will talk to */
 	private static final int oldLastGoodBuild = 1412;
