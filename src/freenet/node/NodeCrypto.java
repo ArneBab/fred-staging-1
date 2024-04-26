00785897aa1ead9454c97ef94af454213655983b
Arne Babenhauserheide <arne_bab@web.de>
2024-04-26 23:26:46 +0200

    Remove no longer necessary pre-sizing of array and doubled generics

--- src/freenet/node/NodeCrypto.java
+++ src/freenet/node/NodeCrypto.java
@@ -593,12 +593,12 @@ public class NodeCrypto {
 	}
 
 	public PeerNode[] getAnonSetupPeerNodes() {
-		ArrayList<PeerNode> v = new ArrayList<PeerNode>();
+		ArrayList<PeerNode> v = new ArrayList<>();
 		for(PeerNode pn: node.getPeers().myPeers()) {
 			if(pn.handshakeUnknownInitiator() && pn.getOutgoingMangler() == packetMangler)
 				v.add(pn);
 		}
-		return v.toArray(new PeerNode[v.size()]);
+		return v.toArray(new PeerNode[0]);
 	}
 
 	void setPortForwardingBroken() {
