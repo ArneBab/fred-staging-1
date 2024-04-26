ea4e88fa1ac7aeb7ff00d8d19c440ac2cf33364f
Arne Babenhauserheide <arne_bab@web.de>
2024-04-26 23:28:14 +0200

    Collapse duplicated exception handling

--- src/freenet/node/FNPPacketMangler.java
+++ src/freenet/node/FNPPacketMangler.java
@@ -1347,19 +1347,10 @@ public class FNPPacketMangler implements OutgoingPacketMangler {
 			try {
 				seed = new SeedClientPeerNode(ref, node, crypto);
 				// Don't tell tracker yet as we don't have the address yet.
-			} catch (FSParseException e) {
+			} catch (FSParseException | PeerParseException | ReferenceSignatureVerificationException | PeerTooOldException e) {
 				Logger.error(this, "Invalid seed client noderef: "+e+" from "+from, e);
 				return null;
-			} catch (PeerParseException e) {
-				Logger.error(this, "Invalid seed client noderef: "+e+" from "+from, e);
-				return null;
-			} catch (ReferenceSignatureVerificationException e) {
-				Logger.error(this, "Invalid seed client noderef: "+e+" from "+from, e);
-				return null;
-			} catch (PeerTooOldException e) {
-                Logger.error(this, "Invalid seed client noderef: "+e+" from "+from, e);
-                return null;
-            }
+			}
 			if(seed.equals(pn)) {
 				Logger.normal(this, "Already connected to seednode");
 				return pn;
